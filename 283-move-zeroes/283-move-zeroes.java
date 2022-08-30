4bb54c211ed7ab32f968b88cfd64e63a0c7e4db1
Arushi Rautela <writetoarushirautela@gmail.com>
2022-08-30 09:20:39 +0530

    Time: 2 ms (83.05%), Space: 54.7 MB (60.57%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,14 +1,15 @@
-public class Solution {
-    public void moveZeroes(int[] nums) {
-        if (nums == null || nums.length == 0) return;
-        
-        int p1 = 0, p2 = 0;
-        while (p1 < nums.length) {
-            while (p2 < nums.length && nums[p2] != 0) p2++;
-            while (p1 < nums.length && (nums[p1] == 0 || p1 < p2)) p1++;
-            if (p1 == nums.length) return;
-            nums[p2] = nums[p1];
-            nums[p1] = 0;
+class Solution {
+     public void moveZeroes(int[] nums) {
+        int snowBallSize = 0; 
+        for (int i=0;i<nums.length;i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
+            }
+            else if (snowBallSize > 0) {
+	            int t = nums[i];
+	            nums[i]=0;
+	            nums[i-snowBallSize]=t;
+            }
         }
     }
 }
